33a5d76937b4d27db8bf44e58d67373e7e0517f1
Kuldeep <111103707+KD-Suthar@users.noreply.github.com>
2024-03-03 16:13:07 +0530

    Add files via upload
    This is the classic implementation of the Hash Table using Python, it does not handle the collision of the key if you want to check that please check hashTableUpdate.py in same Python-DS repo.

---
+++ hashTable.py
@@ -0,0 +1,49 @@
+"""This particular HashMap function does not handle the collision of Key 
+    To handle that please check hashTableUpdate.py in same repo"""
+class HashTale:
+    def __init__(self):
+        self.MAX=100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self,key):
+        h = 0
+        for x in key:
+            h += ord(x)
+        return h % self.MAX
+    
+    """def add(self,key,value):
+        h = self.get_hash(key)
+        self.arr[h] = value
+    
+    def get(self,key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    
+    def delete(self,key):
+        h = self.get_hash(key)
+        self.arr[h] = None        """
+    
+    #Using python operator [getitem, setitem and delitem] so have a functionality like dict in python
+    def __setitem__(self,key,value):
+        h = self.get_hash(key)
+        self.arr[h] = value
+    
+    def __getitem__(self,key):
+        h = self.get_hash(key)
+        return self.arr[h]
+    
+    def __delitem__(self,key):
+        h = self.get_hash(key)
+        self.arr[h] = None
+
+if __name__ == '__main__':
+    table = HashTale()
+
+    """table.add('march 6',320)
+    print(table.get('march 6'))"""
+    table['march 9'] = 190
+    table['march 20'] = 290
+    print(table.arr)
+    del table['march 9']
+    print(table.arr)
+    print(table['march 20'])
